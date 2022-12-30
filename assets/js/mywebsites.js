e1f4aee8070bdd47918ca30c7d073184e1010e30
Martin Tsindi <matefortechnology@gmail.com>
2022-12-30 19:45:43 +0200

    graphs added

---
+++ assets/js/mywebsites.js
@@ -0,0 +1,153 @@
+const ctx = document.getElementById("sacredChart").getContext("2d");
+
+const sacredChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Html", "Css", "Js"],
+    datasets: [
+      {
+        label: "Composition of HTML, CSS and Javascript",
+        data: [70, 22, 8],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom",
+      },
+    },
+  },
+});
+
+/*
+const config = {
+  type: "doughnut",
+  data: data,
+  options: {
+    plugins: {
+      customCanvasBackgroundColor: {
+        color: "lightGreen",
+      },
+    },
+  },
+};
+
+const frst = document.getElementById("forkifyChart");
+
+const forkifyChart = new forkifyChart(frst, config);
+*/
+
+const mtx = document.getElementById("forkifyChart").getContext("2d");
+
+const forkifyChart = new Chart(mtx, {
+  type: "doughnut",
+  data: {
+    labels: ["Html", "Css", "Js"],
+    datasets: [
+      {
+        label: "Composition of HTML, CSS and Javascript",
+        data: [50, 25, 25],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom",
+      },
+    },
+  },
+});
+
+const myctx = document.getElementById("myChart").getContext("2d");
+
+const yelpcamp = new Chart(myctx, {
+  type: "bar",
+  data: {
+    labels: ["Html", "Css", "Js"],
+    datasets: [
+      {
+        label: "Composition of HTML, CSS and Javascript",
+        data: [50, 25, 25],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom",
+      },
+    },
+  },
+});
+
+const nyctx = document.getElementById("idChart").getContext("2d");
+
+const dice = new Chart(nyctx, {
+  type: "bar",
+  data: {
+    labels: ["Html", "Css", "Js"],
+    datasets: [
+      {
+        label: "Composition of HTML, CSS and Javascript",
+        data: [50, 25, 25],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom",
+      },
+    },
+  },
+});
